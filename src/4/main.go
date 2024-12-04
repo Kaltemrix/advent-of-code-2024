30e8ec17c0c716f4edeb31bcc945bce8e0cf7e72
Jacob Flynn <jacobflynn1996@hotmail.com>
2024-12-04 21:42:52 +1000

    Day 4 part 2

--- src/4/main.go
+++ src/4/main.go
@@ -2,151 +2,8 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	"math"
 	"os"
-	"regexp"
 )
-
-type WordSearch struct {
-	wordLines []string
-	xmasCount int
-	widerBy   int
-}
-
-func (ws *WordSearch) FindAllXMASInLines(lines []string) int {
-	regex, err := regexp.Compile(`XMAS`)
-	if err != nil {
-		panic(err)
-	}
-	count := 0
-	for _, line := range lines {
-		lineCopy := line
-		// Make a copy of the line, check for "XMAS", then reverse the line and check for "XMAS" again
-		matches := regex.FindAllString(lineCopy, -1)
-		count += len(matches)
-		// Reverse the line
-		reversedLine := ""
-		for i := len(lineCopy) - 1; i >= 0; i-- {
-			reversedLine += string(lineCopy[i])
-		}
-		matches = regex.FindAllString(reversedLine, -1)
-		count += len(matches)
-	}
-	return count
-}
-
-func (ws *WordSearch) FindAllXMASHorizontal() {
-	count := ws.FindAllXMASInLines(ws.wordLines)
-	ws.xmasCount += count
-}
-
-func (ws *WordSearch) FindAllXMASVertical() {
-	width := len(ws.wordLines)
-	verticalLines := make([]string, width)
-	for _, line := range ws.wordLines {
-		for i := 0; i < width; i++ {
-			verticalLines[i] += string(line[i])
-		}
-	}
-
-	count := ws.FindAllXMASInLines(verticalLines)
-	ws.xmasCount += count
-}
-
-func (ws *WordSearch) FindAllXMASDiagonalTopLeftToBottomRight() {
-	width := len(ws.wordLines[0]) - ws.widerBy
-	if ws.widerBy < 0 {
-		width = len(ws.wordLines) + ws.widerBy
-	}
-
-	diagonalLines := []string{}
-
-	for i := 0; i < width; i++ {
-		thisLine := ""
-		for j := 0; j <= i; j++ {
-			thisLine += string(ws.wordLines[j][width-i+j-1])
-		}
-		diagonalLines = append(diagonalLines, thisLine)
-	}
-
-	if ws.widerBy > 0 {
-		for i := 0; i < ws.widerBy; i++ {
-			thisLine := ""
-			for j := 0; j < width; j++ {
-				thisLine += string(ws.wordLines[j][ws.widerBy-i+j-1])
-			}
-			diagonalLines = append(diagonalLines, thisLine)
-		}
-	} else if ws.widerBy < 0 {
-		absWiderBy := int(math.Abs(float64(ws.widerBy)))
-		for i := 0; i < absWiderBy; i++ {
-			thisLine := ""
-			for j := 0; j < width; j++ {
-				thisLine += string(ws.wordLines[absWiderBy+i+j-1][j])
-			}
-			diagonalLines = append(diagonalLines, thisLine)
-		}
-	}
-
-	for i := 0; i < width-1; i++ {
-		thisLine := ""
-		for j := 0; j <= i; j++ {
-			thisLine += string(ws.wordLines[len(ws.wordLines)-i+j-1][j])
-		}
-		diagonalLines = append(diagonalLines, thisLine)
-	}
-
-	count := ws.FindAllXMASInLines(diagonalLines)
-	ws.xmasCount += count
-}
-
-func (ws *WordSearch) FindAllXMASDiagonalTopRightToBottomLeft() {
-	width := len(ws.wordLines[0]) - ws.widerBy
-	if ws.widerBy < 0 {
-		width = len(ws.wordLines) + ws.widerBy
-	}
-
-	diagonalLines := []string{}
-
-	for i := 0; i < width; i++ {
-		thisLine := ""
-		for j := 0; j <= i; j++ {
-			thisLine += string(ws.wordLines[j][i-j])
-		}
-		diagonalLines = append(diagonalLines, thisLine)
-	}
-
-	if ws.widerBy > 0 {
-		for i := 0; i < ws.widerBy; i++ {
-			thisLine := ""
-			for j := 0; j < width; j++ {
-				thisLine += string(ws.wordLines[j][width+i-j])
-			}
-			diagonalLines = append(diagonalLines, thisLine)
-		}
-	} else if ws.widerBy < 0 {
-		absWiderBy := int(math.Abs(float64(ws.widerBy)))
-		for i := 0; i < absWiderBy; i++ {
-			thisLine := ""
-			for j := 0; j < width; j++ {
-				thisLine += string(ws.wordLines[absWiderBy+i+j][j])
-			}
-			diagonalLines = append(diagonalLines, thisLine)
-		}
-	}
-
-	for i := 0; i < width-1; i++ {
-		thisLine := ""
-		for j := 0; j <= i; j++ {
-			thisLine += string(ws.wordLines[width-i+j-1][width-j-1])
-		}
-		diagonalLines = append(diagonalLines, thisLine)
-	}
-
-	count := ws.FindAllXMASInLines(diagonalLines)
-	ws.xmasCount += count
-}
 
 func main() {
 	file, err := os.Open("data.txt")
@@ -155,21 +12,57 @@ func main() {
 	}
 	defer file.Close()
 
-	wordSearch := WordSearch{}
+	lines := make([]string, 0)
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		wordSearch.wordLines = append(wordSearch.wordLines, line)
+		lines = append(lines, line)
 	}
 
-	// Lets find out if the search is wider or taller, and by how much
-	widerBy := len(wordSearch.wordLines[0]) - len(wordSearch.wordLines)
-	wordSearch.widerBy = widerBy
+	// Scanning each character of each line
+	// If we encounter a M, check if line+1 index+1 is A, and line+2 index+2 is S
+	//  Next check if line+0 index+2 is an M or an S
+	//  If it's an M, check if line+2 index+0 is an S
+	//  If it's an S, check if line+2 index+0 is an M
+	// If all checks pass, increment the count
+	// If we encounter an S, check if line+1 index+1 is an A, and line+2 index+2 is an M
+	//  Next check if line+0 index+2 is an S or an M
+	//  If it's an S, check if line+2 index+0 is an M
+	//  If it's an M, check if line+2 index+0 is an S
+	// If all checks pass, increment the count
+	// Also, we need to check if we're not going out of bounds
+	// If we do, continue to the next iteration
 
-	wordSearch.FindAllXMASHorizontal()
-	wordSearch.FindAllXMASVertical()
-	wordSearch.FindAllXMASDiagonalTopLeftToBottomRight()
-	wordSearch.FindAllXMASDiagonalTopRightToBottomLeft()
+	count := 0
 
-	fmt.Println(wordSearch.xmasCount)
+	for i := 0; i < len(lines); i++ {
+		for j := 0; j < len(lines[i]); j++ {
+			if j+2 < len(lines[i]) && i+2 < len(lines) {
+				if lines[i][j] == 'M' && lines[i+1][j+1] == 'A' && lines[i+2][j+2] == 'S' {
+					if lines[i][j+2] == 'M' {
+						if lines[i+2][j] == 'S' {
+							count++
+						}
+					} else if lines[i][j+2] == 'S' {
+						if lines[i+2][j] == 'M' {
+							count++
+						}
+					}
+				} else if lines[i][j] == 'S' && lines[i+1][j+1] == 'A' && lines[i+2][j+2] == 'M' {
+					if lines[i][j+2] == 'S' {
+						if lines[i+2][j] == 'M' {
+							count++
+						}
+					} else if lines[i][j+2] == 'M' {
+						if lines[i+2][j] == 'S' {
+							count++
+						}
+					}
+				}
+			}
+		}
+	}
+
+	println(count)
 }
